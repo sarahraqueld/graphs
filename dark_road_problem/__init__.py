e145e18488b8384e3b54abe7dd4dc513e02a8290
Sarah Raquel De Lima Ferreira <sarahraqueld@A305-33L.imd.local>
2017-08-21 13:54:26 -0300

    Print the graph

--- dark_road_problem/__init__.py
+++ dark_road_problem/__init__.py
@@ -5,16 +5,14 @@ import sys
 from readinput import readinput
 r = readinput()
 
-qtdArestas = 0
-qtdVertices = 0
-
 g = Graph(False)
 
 for index, elem in enumerate(r.get_input_file()):
-    if(index == 0):
-        qtdArestas = elem[0]
-        qtdVertices = elem[1]
+    if(index == 0 or (elem[0] == '0' and elem[1] == '0')):
+    	pass
     else:
         v1 = g.add_vertex(Vertex(elem[0]))
         v2 = g.add_vertex(Vertex(elem[1]))
         g.add_edge(v1, v2)
+
+print g
